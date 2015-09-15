9832b20b474b569ca38f3a868aacdc7938fdca81
hitoy <vip@hitoy.org>
2015-09-15 15:56:27 +0800

    add

---
+++ dydns.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+# dnspod 动态更新IP的客户端
+
+import post,json,sys,urllib2,urllib,os,time
+
+def POST(url,data):
+    formdata=urllib.urlencode(data)
+    req = urllib2.Request(url,headers={'User-Agent' : "Magic Browser"}) 
+    f = urllib2.urlopen(req,formdata,timeout=10)
+    content = f.read()
+    return content
+
+def GetIP():
+		return POST("http://lab.hitoy.org/api/gettheip.php","")
+
+
+class dnspod:
+		def __init__(self,username,password,token):
+				self.username=username
+				self.password=password
+				self.token=token
+
+		def get_domain_list(self):
+				dlist = POST("https://dnsapi.cn/Domain.List",{"login_email":self.username,"login_password":self.password,"format":"json"})
+				ddict = json.loads(dlist)
+				self.domains=ddict['domains']
+
+		def get_record_list(self,domain):
+				domain_id=0
+				for d in self.domains:
+						if d['punycode']  == domain:
+								domain_id=d['id']
+				if domain_id == 0:return
+				rlist = POST("https://dnsapi.cn/Record.List",{"login_email":self.username,"login_password":self.password,"format":"json","domain_id":domain_id})
+				return json.loads(rlist)
+
+		def update_record(self,sub_domain,value,retype="A",reline="默认",ttl=600,mx=10):
+				self.get_domain_list()
+				record = self.get_record_list("hitoy.org")
+				domain_id=record['domain']['id']
+				for i in record['records']:
+						if i['name'] == sub_domain:
+								record_id = i['id']
+
+				do = POST("https://dnsapi.cn/Record.Modify",{"login_email":self.username,"login_password":self.password,"format":"json","domain_id":domain_id,"record_id":record_id,"sub_domain":sub_domain,"value":value,"record_type":retype,"record_line":reline,"ttl":ttl})
+				resulte = json.loads(do)
+				return resulte['status']['message']
+
+
+ip = "1.1.1.1"
+uname = "example@hitoy.org"
+password = "example"
+sub_domain = "www"
+
+dns = dnspod(uname,password,"")
+while True:
+		if GetIP() != ip:
+				ip = GetIP()
+				re  = dns.update_record(sub_domain,ip)
+				sys.stdout.write("%s\r"%re)
+		time.sleep(20)
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Dydns 动态更新域名DNS
+
+##使用方法
+* 打开dydns.py文件，更改uname,password为你的dnspod帐号和密码，更改sub_domain为你的二级域
+* 运行此文件即可
